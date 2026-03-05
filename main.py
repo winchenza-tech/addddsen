089daaa1be248aaf819d708aa191b4df13f5b710
winchenza-tech <winchenza@gmail.com>
2026-03-05 12:35:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
 TELEGRAM_TOKEN = "8637130007:AAHwNRSwfjZQcfYDoGNKWjuIiBYB8at8fvI"
-TARGET_BOT_ID = 5177820294
+# Artık birden fazla ID'yi hedef alabiliyoruz
+TARGET_BOT_IDS = [5177820294, 8416720490] 
 BANNED_KEYWORDS = [
     "kanalımıza", "kanalına", "grubuna", "grubumuza", "davetlisiniz"
 ]
@@ -14,11 +15,13 @@ async def delete_octopus_ads(update: Update, context: ContextTypes.DEFAULT_TYPE)
     
     if not msg or not user:
         return
-    if user.id == TARGET_BOT_ID:
+        
+    # Kullanıcı ID'si hedef listemizin içinde mi diye kontrol ediyoruz
+    if user.id in TARGET_BOT_IDS:
         text = msg.text or msg.caption or ""
         text_lower = text.lower()
  
-        # https://, http:// olsa da olmasa da tüm t.me/ linklerini acımadan yakalar
+        # https://, http:// olsa da olmasa da tüm t.me/ linklerini yakalar
         has_link = bool(re.search(r'(?:https?://)?t\.me/\S+', text_lower))
  
         has_keyword = any(keyword in text_lower for keyword in BANNED_KEYWORDS)
@@ -26,7 +29,7 @@ async def delete_octopus_ads(update: Update, context: ContextTypes.DEFAULT_TYPE)
         if has_link or has_keyword:
             try:
                 await msg.delete()
-                print(f"Geçici test mesajı: Reklam engellendi. ")
+                print(f"Geçici test mesajı: {user.id} ID'li hedeften gelen reklam engellendi.")
             except Exception as e:
                 print(f"Mesaj silinemedi (Botun grupta admin yetkisi eksik olabilir): {e}")
 
@@ -35,7 +38,7 @@ def main():
     
     app.add_handler(MessageHandler(filters.ALL, delete_octopus_ads))
     
-    print("Anti-Octopus Botu çalışıyor... Hedef kilitlendi.")
+    print("Anti-Reklam Botu çalışıyor... Hedefler kilitlendi.")
     app.run_polling()
 
 if __name__ == "__main__":
